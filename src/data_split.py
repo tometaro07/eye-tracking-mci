9340d2b051a9f7c2533a1237cbaee5f27388d7e2
tometaro07 <93013897+tometaro07@users.noreply.github.com>
2024-10-31 21:59:19 +0000

    Update data_split.py

--- src/data_split.py
+++ src/data_split.py
@@ -4,40 +4,22 @@ import torch
 
 from data_tools import *
 
-PROCESS_DIR = '/home/tometaro/Documents/Thesis/processed_data/processed/first/'
-DATASET_DIR = '/home/tometaro/Documents/Thesis/datasets/VTNet/'
+PROCESS_DIR = ''
+DATASET_DIR = ''
 
-dataset=VTNet_Dataset(path=PROCESS_DIR)
+dataset=VTNet_Dataset(path=PROCESS_DIR,useHeatmap=True)
 
-with open(f'{DATASET_DIR}data.pkl', 'wb') as file:
+with open(f'{DATASET_DIR}VTNet_heatmap/data.pkl', 'wb') as file:
     pickle.dump(dataset, file)
 
+guides = dataset.doubleStratifiedSplit()
 
-t,v,tt = dataset.doubleStratifiedSplit(downsample=True)
-train_set = VTNet_Dataset(scanpaths=dataset.scanpaths[t,:,:,:], rawdata=dataset.rawdata[t], subject=dataset.subject[t], groups=dataset.groups[t])
-val_set = VTNet_Dataset(scanpaths=dataset.scanpaths[v,:,:,:], rawdata=dataset.rawdata[v], subject=dataset.subject[v], groups=dataset.groups[v])
-test_set = VTNet_Dataset(scanpaths=dataset.scanpaths[tt,:,:,:], rawdata=dataset.rawdata[tt], subject=dataset.subject[tt], groups=dataset.groups[tt])
+with open(f'{DATASET_DIR}guides.pkl', 'wb') as file:
+    pickle.dump(guides, file)
 
-mean_pupil_1 = torch.mean(train_set.rawdata[:,:,3])
-std_pupil_1 = torch.std(train_set.rawdata[:,:,3])
+del dataset, guides
 
-mean_pupil_2 = torch.mean(train_set.rawdata[:,:,6])
-std_pupil_2 = torch.std(train_set.rawdata[:,:,6])
+dataset2= VTNet_Dataset(path=PROCESS_DIR)
 
-train_set.rawdata[:,:,3] = (train_set.rawdata[:,:,3]-mean_pupil_1)/std_pupil_1
-train_set.rawdata[:,:,6] = (train_set.rawdata[:,:,6]-mean_pupil_2)/std_pupil_2
-
-test_set.rawdata[:,:,3] = (test_set.rawdata[:,:,3]-mean_pupil_1)/std_pupil_1
-test_set.rawdata[:,:,6] = (test_set.rawdata[:,:,6]-mean_pupil_2)/std_pupil_2
-
-val_set.rawdata[:,:,3] = (val_set.rawdata[:,:,3]-mean_pupil_1)/std_pupil_1
-val_set.rawdata[:,:,6] = (val_set.rawdata[:,:,6]-mean_pupil_2)/std_pupil_2
-
-with open(f'{DATASET_DIR}trainset_vtnet.pkl', 'wb') as file:
-    pickle.dump(train_set, file)
-    
-with open(f'{DATASET_DIR}valset_vtnet.pkl', 'wb') as file:
-    pickle.dump(val_set, file)
-    
-with open(f'{DATASET_DIR}testset_vtnet.pkl', 'wb') as file:
-    pickle.dump(test_set, file)
+with open(f'{DATASET_DIR}VTNet/data.pkl', 'wb') as file:
+    pickle.dump(dataset2, file)
